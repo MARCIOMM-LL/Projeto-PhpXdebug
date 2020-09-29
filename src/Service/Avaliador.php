0f9eb24580abd89e54b8842065253aadda1eaf06
Márcio <m-andremiranda@outlook.com>
2020-09-29 19:47:53 -0300

    add

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -57,7 +57,7 @@ class Avaliador
     {
         $lances = $leilao->getLances();
         usort($lances, function (Lance $lance1, Lance $lance2) {
-            return $lance2->getValor() - $lance1->getValor();
+            return $lance2->getValor() - $lance1->getValor(); //Devolve um número negativo se o primeiro item vier antes no array do segundo item, retorna um número negativo se o lance1 vier primeiro, retorna 0 se eles forem iguais e maior do que 0 se o lance2 tiver que vir primeiro. O lance1 é equivalante ao negativo e - lance2 é equivalente ao 0 positivo, seguindo esse raciocínio, se retornarmos o lance1 menos o lance2, teremos uma ordenação do array em ordem crescente, se ordenarmos o lance2 menos o lance1, teremos uma ordenação decrescente no array de valores
         });
 
         return array_slice($lances, 0, 3);
